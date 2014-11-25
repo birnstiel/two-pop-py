b6d90ee5421b377e0ccf2cb53e56fd7f0131b896
Til Birnstiel <birnstiel@me.com>
2014-11-25 17:50:58 -0500

    cleaned up the directory from files not needed in this stand-alone-version

--- src/get_sims.py
+++
@@ -1,4 +0,0 @@
-def get_sims():
-    return [
-            '~/Desktop/new_results/hubert/two-pop-models/data_driftvar2011_MD01_RC60_VF10_AL-3_161.mat',
-            '~/Desktop/new_results/hubert/two-pop-models/data_driftvar2011_MD01_RC60_VF10_AL-2_121.mat']
