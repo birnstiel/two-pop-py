2b5492139e8cec9bd0eac3cd452cf7189814bf4b
Til Birnstiel <birnstiel@me.com>
2016-08-29 21:34:14 +0200

    changed everything to a module architecture

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='twopoppy',
+    version='0.1',
+    description='two-population dust evolution model according to Birnstiel, Klahr, Ercolano, A&A (2012)',
+    long_descrioption=read('README.md'),
+    url='http://www.til-birnstiel.de',
+    author='Til Birnstiel',
+    author_email='birnstiel@me.com',
+    license='GPLv3',
+    packages=['twopoppy'],
+    include_package_data=True,
+    install_requires=[
+        'astropy',
+        'scipy',
+        'numpy',
+        'matplotlib'
+        ],
+    zip_safe=False)
