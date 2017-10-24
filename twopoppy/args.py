a889238aefc74c3c0e1cb2b87035dcc6387a9a75
birnstiel@me.com <birnstiel@me.com>
2017-10-24 12:26:47 +0200

    args now pep8 conform

--- twopoppy/args.py
+++ twopoppy/args.py
@@ -1,63 +1,63 @@
 class args:
     from . import const as _c
-    
+
     # names of all parameters
 
-    varlist = [ ['nr',        int],
-                ['nt',        int],
-                ['tmax',    float],
-                ['alpha',   float],
-                ['d2g',     float],
-                ['mstar',   float],
-                ['tstar',   float],
-                ['rstar',   float],
-                ['rc',      float],
-                ['rt',      float],
-                ['r0',      float],
-                ['r1',      float],
-                ['mdisk',   float],
-                ['rhos',    float],
-                ['vfrag',   float],
-                ['a0',      float],
-                ['gamma',   float],
-                ['edrift',  float],
-                ['estick',  float],
-                ['T',       float],
-                ['gasevol',  bool],
-                ['tempevol', bool],
-                ['starevol', bool],
+    varlist = [ ['nr',        int],  # noqa
+                ['nt',        int],  # noqa
+                ['tmax',    float],  # noqa
+                ['alpha',   float],  # noqa
+                ['d2g',     float],  # noqa
+                ['mstar',   float],  # noqa
+                ['tstar',   float],  # noqa
+                ['rstar',   float],  # noqa
+                ['rc',      float],  # noqa
+                ['rt',      float],  # noqa
+                ['r0',      float],  # noqa
+                ['r1',      float],  # noqa
+                ['mdisk',   float],  # noqa
+                ['rhos',    float],  # noqa
+                ['vfrag',   float],  # noqa
+                ['a0',      float],  # noqa
+                ['gamma',   float],  # noqa
+                ['edrift',  float],  # noqa
+                ['estick',  float],  # noqa
+                ['T',       float],  # noqa
+                ['gasevol',  bool],  # noqa
+                ['tempevol', bool],  # noqa
+                ['starevol', bool],  # noqa
             ]
-    
-    # set default values
-    
-    nr      = 200
-    nt      = 100
-    na      = 150
-    tmax    = 1e6*_c.year
-    alpha   = 1e-3
-    d2g     = 1e-2
-    mstar   = 0.7*_c.M_sun
-    tstar   = 4010.
-    rstar   = 1.806*_c.R_sun
-    rc      = 200*_c.AU
-    rt      = 1e6*_c.AU
-    r0      = 0.05*_c.AU
-    r1      = 3e3*_c.AU
-    mdisk   = 0.1*mstar
-    rhos    = 1.156
-    vfrag   = 1000
-    a0      = 1e-5
-    gamma   = 1.0
-    edrift  = 1.0
-    estick  = 1.0
 
-    gasevol  = True
-    tempevol = False
-    starevol = False
-    T        = None
-    dir      = 'data'
-    
-    def __init__(self,**kwargs):
+    # set default values
+
+    nr      = 200               # noqa
+    nt      = 100               # noqa
+    na      = 150               # noqa
+    tmax    = 1e6*_c.year       # noqa
+    alpha   = 1e-3              # noqa
+    d2g     = 1e-2              # noqa
+    mstar   = 0.7*_c.M_sun      # noqa
+    tstar   = 4010.             # noqa
+    rstar   = 1.806*_c.R_sun    # noqa
+    rc      = 200*_c.AU         # noqa
+    rt      = 1e6*_c.AU         # noqa
+    r0      = 0.05*_c.AU        # noqa
+    r1      = 3e3*_c.AU         # noqa
+    mdisk   = 0.1*mstar         # noqa
+    rhos    = 1.156             # noqa
+    vfrag   = 1000              # noqa
+    a0      = 1e-5              # noqa
+    gamma   = 1.0               # noqa
+    edrift  = 1.0               # noqa
+    estick  = 1.0               # noqa
+
+    gasevol  = True   # noqa
+    tempevol = False  # noqa
+    starevol = False  # noqa
+    T        = None   # noqa
+    dir      = 'data' # noqa
+
+    def __init__(self, **kwargs):
         """
         Initialize arguments. Simulation parameters can be given as keywords.
         To list all parameters, call `print_args()`. All quantities need to be
@@ -65,102 +65,106 @@ class args:
         using other units.
         """
         import warnings
-        for k,v in kwargs.items():
-            if hasattr(self,k):
-                setattr(self,k,v)
+        for k, v in kwargs.items():
+            if hasattr(self, k):
+                setattr(self, k, v)
             else:
                 warnings.warn("No such argument")
-    
+
     def __str__(self):
         """
         String representation of arguments
         """
         from .const import year, M_sun, R_sun, AU
         from numbers import Number
-        
+
         s = ''
-        s+=48*'-'+'\n'
-        
+        s += 48 * '-' + '\n'
+
         conversion = {
-            'nr':      [1,            ''],
-            'nt':      [1,            ''],
-            'tmax':    [1/year,       'years'],
-            'd2g':     [1,            ''],
-            'mstar':   [1/M_sun,      'solar masses'],
-            'tstar':   [1,            'K'],
-            'rstar':   [1/R_sun,      'R_sun'],
-            'rc':      [1/AU,         'AU'],
-            'rt':      [1/AU,         'AU'],
-            'r0':      [1/AU,         'AU'],
-            'r1':      [1/AU,         'AU'],
-            'mdisk':   [1/self.mstar, 'M_star'],
-            'rhos':    [1,            'g/cm^3'],
-            'vfrag':   [1,            'cm/s'],
-            'a0':      [1,            'cm'],
-            'gamma':   [1,            ''],
-            'edrift':  [1,            ''],
-            'estick':  [1,            '']
+            'nr':      [1,            ''],              # noqa
+            'nt':      [1,            ''],              # noqa
+            'tmax':    [1/year,       'years'],         # noqa
+            'd2g':     [1,            ''],              # noqa
+            'mstar':   [1/M_sun,      'solar masses'],  # noqa
+            'tstar':   [1,            'K'],             # noqa
+            'rstar':   [1/R_sun,      'R_sun'],         # noqa
+            'rc':      [1/AU,         'AU'],            # noqa
+            'rt':      [1/AU,         'AU'],            # noqa
+            'r0':      [1/AU,         'AU'],            # noqa
+            'r1':      [1/AU,         'AU'],            # noqa
+            'mdisk':   [1/self.mstar, 'M_star'],        # noqa
+            'rhos':    [1,            'g/cm^3'],        # noqa
+            'vfrag':   [1,            'cm/s'],          # noqa
+            'a0':      [1,            'cm'],            # noqa
+            'gamma':   [1,            ''],              # noqa
+            'edrift':  [1,            ''],              # noqa
+            'estick':  [1,            '']               # noqa
             }
-        
-        for n,conv_unit in conversion.items():
-            conv,unit = conv_unit
-            value     = getattr(self, n)
-            isarray   = hasattr(value, '__len__')
+
+        for n, conv_unit in conversion.items():
+            conv, unit = conv_unit
+            value = getattr(self, n)
+            isarray = hasattr(value, '__len__')
             if isarray:
-                s+=n.ljust(17)+' = '+'[{:3.2g} ... {:3.2g}]'.format(conv*value[0],conv*value[-1]).rjust(15)+' '+unit+'\n'
+                s += n.ljust(17) + ' = ' + '[{:3.2g} ... {:3.2g}]'.format(conv * value[0], conv * value[-1]).rjust(15) + ' ' + unit + '\n'
             else:
-                s+=n.ljust(17)+' = '+'{:3.2g}'.format(conv*value).rjust(15)+' '+unit+'\n'
-            
+                s += n.ljust(17) + ' = ' + '{:3.2g}'.format(conv * value).rjust(15) + ' ' + unit + '\n'
+
         # print other arguments
-            
-        s += 'Gas         evol.'.ljust(17)+' = '+(self.gasevol *'on'+(not self.gasevol )*'off').rjust(15)+'\n'
-        s += 'Temperature evol.'.ljust(17)+' = '+(self.tempevol*'on'+(not self.tempevol)*'off').rjust(15)+'\n'
-        s += 'Stellar     evol.'.ljust(17)+' = '+(self.starevol*'on'+(not self.starevol)*'off').rjust(15)+'\n'
+
+        s += 'Gas         evol.'.ljust(17) + ' = ' + (self.gasevol * 'on' + (not self.gasevol) * 'off').rjust(15) + '\n'
+        s += 'Temperature evol.'.ljust(17) + ' = ' + (self.tempevol * 'on' + (not self.tempevol) * 'off').rjust(15) + '\n'
+        s += 'Stellar     evol.'.ljust(17) + ' = ' + (self.starevol * 'on' + (not self.starevol) * 'off').rjust(15) + '\n'
 
         # print temperature
-        
+
         if self.T is None:
-            s += 'Temperature'.ljust(17)+' = '+'None'.rjust(15)
-        elif hasattr(self.T,'__call__'):
-            s += 'Temperature'.ljust(17)+' = '+'function'.rjust(15)
+            s += 'Temperature'.ljust(17) + ' = ' + 'None'.rjust(15)
+        elif hasattr(self.T, '__call__'):
+            s += 'Temperature'.ljust(17) + ' = ' + 'function'.rjust(15)
         else:
-            s += 'Temperature'.ljust(17)+' = '+'{}'.format(self.T).rjust(15)
-        s+='\n'
-            
+            s += 'Temperature'.ljust(17) + ' = ' + '{}'.format(self.T).rjust(15)
+        s += '\n'
+
         # print alpha
 
-        if isinstance(self.alpha,Number):
-            s += 'alpha'.ljust(17)+' = '+'{}'.format(self.alpha).rjust(15)
-        elif hasattr(self.alpha,'__call__'):
-            s += 'alpha'.ljust(17)+      ' = '+'function'.rjust(15)
+        if isinstance(self.alpha, Number):
+            s += 'alpha'.ljust(17) + ' = ' + '{}'.format(self.alpha).rjust(15)
+        elif hasattr(self.alpha, '__call__'):
+            s += 'alpha'.ljust(17) + ' = ' + 'function'.rjust(15)
         else:
-            s += 'alpha'.ljust(17)+' = '+'{}'.format(self.alpha).rjust(15)
-        
-        s += '\n'+48*'-'+'\n'
+            s += 'alpha'.ljust(17) + ' = ' + '{}'.format(self.alpha).rjust(15)
+
+        s += '\n' + 48 * '-' + '\n'
         return s
-        
+
     def print_args(self):
         print(self.__str__())
-    
-    def write_args(self,fname=None):
+
+    def write_args(self, fname=None):
         """
         Write out the simulation parameters to the file 'parameters.ini' in the
         folder specified in args.dir or to fname if that is not None
         """
-        import configobj, os
+        import configobj
+        import os
+
         if fname is None:
             folder = self.dir
-            fname  = 'parameters.ini'
+            fname = 'parameters.ini'
         else:
-            fname  = os.path.abspath(os.path.expanduser(fname))
+            fname = os.path.abspath(os.path.expanduser(fname))
             folder = os.path.dirname(fname)
-            fname  = os.path.basename(fname)
-            
-        if not os.path.isdir(folder): os.mkdir(folder)
-        parser = configobj.ConfigObj()
-        parser.filename = os.path.join(folder,fname)
+            fname = os.path.basename(fname)
 
-        for name,_ in self.varlist: parser[name] = getattr(self, name)
+        if not os.path.isdir(folder):
+            os.mkdir(folder)
+        parser = configobj.ConfigObj()
+        parser.filename = os.path.join(folder, fname)
+
+        for name, _ in self.varlist:
+            parser[name] = getattr(self, name)
 
         parser.write()
 
@@ -169,42 +173,45 @@ class args:
         Read in the simulation parameters from the file 'parameters.ini' in the
         folder specified in args.dir
         """
-        import configobj, os, numpy
-        parser = configobj.ConfigObj(self.dir+os.sep+'parameters.ini')
-        
-        varlist = {v[0]:v[1] for v in self.varlist}
-        
-        for name,val in parser.items():
+        import configobj
+        import os
+        import numpy as np
+
+        parser = configobj.ConfigObj(self.dir + os.sep + 'parameters.ini')
+
+        varlist = {v[0]: v[1] for v in self.varlist}
+
+        for name, val in parser.items():
             if name not in varlist:
                 print('Unknown Parameter:{}'.format(name))
                 continue
-            
+
             t = varlist[name]
-            
+
             # process ints, bools, floats and lists of them
-            
-            if t in [int,bool,float]:
+
+            if t in [int, bool, float]:
                 if type(val) is list:
                     # lists
                     setattr(self, name, [t(v) for v in val])
                 elif '[' in val:
                     # numpy arrays
-                    val = val.replace('[','').replace(']','').replace('\n',' ')
+                    val = val.replace('[', '').replace(']', '').replace('\n', ' ')
                     val = [t(v) for v in val.split()]
-                    setattr(self, name, numpy.array(val))
+                    setattr(self, name, np.array(val))
                 else:
                     try:
                         # plain values
                         setattr(self, name, t(val))
                     except:
                         try:
-                            setattr(self,name,val)
+                            setattr(self, name, val)
                         except:
-                            print('Could not convert variable \'{}\' with stored value {}'.format(name,t(val)))
-                            
+                            print('Could not convert variable \'{}\' with stored value {}'.format(name, t(val)))
+
             else:
                 # stings and nones
-                if val=='None':
+                if val == 'None':
                     val = None
                 else:
-                    setattr(self,name,val)
+                    setattr(self, name, val)
