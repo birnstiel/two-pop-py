d75200f0c1890dad55aca70bacd8ad98668605bb
birnstiel@me.com <birnstiel@me.com>
2017-11-02 09:27:51 +0100

    updated way to set version of the package

--- twopoppy/__init__.py
+++ twopoppy/__init__.py
@@ -1,10 +1,17 @@
 """Initiazlization file for twopoppy"""
 __all__ = ['const', 'model', 'args', 'wrapper', 'model_wrapper']
-from setuptools_scm import get_version
+#
+# get version
+#
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
+
 from .wrapper import model_wrapper
 from .args import args
 from . import const
 from . import model
 from . import wrapper
-
-__version__ = get_version(root='..', relative_to=__file__)
