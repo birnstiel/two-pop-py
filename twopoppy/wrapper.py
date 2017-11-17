9b1d781e5d480fae86d162fa0c937a992bdf340a
birnstiel@me.com <birnstiel@me.com>
2017-11-17 09:58:12 +0100

    minor fixes in the wrapper for using time dependent temperatures

--- twopoppy/wrapper.py
+++ twopoppy/wrapper.py
@@ -393,7 +393,7 @@ def model_wrapper(ARGS, plot=False, save=False):
         print('reconstructing size distribution')
         it = -1
         sig_sol, _, _, _, _, _ = reconstruct_size_distribution(
-            x, a, TI[it], SOLG[it], SOLD[-1], alpha * np.ones(nr), rhos, T, mstar, vfrag, a_0=a0)
+            x, a, TI[it], SOLG[it], SOLD[it], alpha * np.ones(nr), rhos, Tout[it], mstar, vfrag, a_0=a0)
 
     except Exception:
         import traceback
